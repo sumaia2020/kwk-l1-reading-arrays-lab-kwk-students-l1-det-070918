1e22688a49e8df633598811e8f3a34214cc9b201
sumaia2020 <sbalghaiti@hawkmail.hfcc.edu>
2018-07-11 20:27:13 +0000

    automatically backed up by learn

--- test.rb
+++ test.rb
@@ -1,6 +1,6 @@
 
 def first_student_by_index
   student_names = ["Adele","Beyoncé", "Cardi B", "Lady Gaga","Nicki Minaj","Rihanna"]
-returns student_names[0]
+putsstudent_names[0]
 end
 first_student_by_index
