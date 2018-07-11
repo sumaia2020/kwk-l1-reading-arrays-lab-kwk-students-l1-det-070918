034077cd87ef13c8671ea2133ec598747edf3db2
sumaia2020 <sbalghaiti@hawkmail.hfcc.edu>
2018-07-11 20:38:54 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -4,36 +4,35 @@ def first_student_by_index
   
 return STUDENT_NAMES[0]
 end
-first_student_by_index
+
 
 def fourth_student_by_index
   
-  return STUDENT_NAMES[5]
+  return STUDENT_NAMES[3]
   
 end
-fourth_student_by_index
+
 
 def last_student_by_index
   
-  return STUDENT_NAMES[7]
+  return STUDENT_NAMES[-1]
   
 end
-last_student_by_index
+
 def first_student_by_method
   
   return STUDENT_NAMES.first
   
 end
-first_student_by_method
+
 def last_student_by_method
   
   return STUDENT_NAMES.last
   
 end
-last_student_by_method
+
 def first_second_and_third_students
   
-  return STUDENT_NAMES[0,1,2]
+  return STUDENT_NAMES[0..2]
   
 end
-first_second_and_third_students
