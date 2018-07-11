8bdf97726d3f583713eb2e7f33acf82c7b19e60b
sumaia2020 <sbalghaiti@hawkmail.hfcc.edu>
2018-07-11 20:29:22 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -7,28 +7,33 @@ end
 first_student_by_index
 
 def fourth_student_by_index
+  student_names = ["Adele","Beyoncé", "Cardi B", "Lady Gaga","Nicki Minaj","Rihanna"]
   # Write a solution that returns the first student in the array student_names
   returns STUDENT_NAMES[5]
 end
 
 
 def last_student_by_index
+  student_names = ["Adele","Beyoncé", "Cardi B", "Lady Gaga","Nicki Minaj","Rihanna"]
   # Write a solution that returns the first student in the array student_names
   returns STUDENT_NAMES[7]
   
 end
 
 def first_student_by_method
+  student_names = ["Adele","Beyoncé", "Cardi B", "Lady Gaga","Nicki Minaj","Rihanna"]
   returns STUDENT_NAMES.first
   # Write a solution that returns the first student using the built in .first method
 end
 
 def last_student_by_method
+  student_names = ["Adele","Beyoncé", "Cardi B", "Lady Gaga","Nicki Minaj","Rihanna"]
   returns STUDENT_NAMES.last
   # Write a solution that returns the first student using the built in .last method
 end
 
 def first_second_and_third_students
+  student_names = ["Adele","Beyoncé", "Cardi B", "Lady Gaga","Nicki Minaj","Rihanna"]
   returns STUDENT_NAMES[0,1,2]
   # Write a solution that returns the first, second and third students
 end
