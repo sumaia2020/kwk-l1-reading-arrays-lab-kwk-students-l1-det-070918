48210fe13f787ebbba51698f11a009e3aea32f36
sumaia2020 <sbalghaiti@hawkmail.hfcc.edu>
2018-07-11 20:22:56 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,32 +1,31 @@
-STUDENT_NAMES = [
-  "Adele",
-  "Beyoncé",
-  "Cardi B",
-  "Lady Gaga",
-  "Nicki Minaj",
-  "Rihanna"
-]
+STUDENT_NAMES = ["Adele","Beyoncé", "Cardi B", "Lady Gaga","Nicki Minaj","Rihanna"]
 
 def first_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  returns STUDENT_NAMES[0]
 end
-
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
+  returns STUDENT_NAMES[5]
 end
+
 
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
+  returns STUDENT_NAMES[7]
+  
 end
 
 def first_student_by_method
+  returns STUDENT_NAMES.first
   # Write a solution that returns the first student using the built in .first method
 end
 
 def last_student_by_method
+  returns STUDENT_NAMES.last
   # Write a solution that returns the first student using the built in .last method
 end
 
 def first_second_and_third_students
+  returns STUDENT_NAMES[0,1,2]
   # Write a solution that returns the first, second and third students
 end
---
+++ spec/test.rb
@@ -0,0 +1,5 @@
+def student
+  student_names["Adele","Beyoncé","Cardi B","Lady Gaga","Nicki Minaj","Rihanna"]
+  puts student_names[0]
+end
+student
